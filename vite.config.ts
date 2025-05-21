f92cc5d945a7a0bb7324ffd891eca9c82e9fbb1d
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2025-05-20 17:37:50 -0700

    Rebuild and remove all dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
  
 export default defineConfig({
   root: 'packages/fika-collect-survey-editor',
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true
+  },
   server: {
     port: process.env.PORT as unknown as number,
   },
