e028ee68fb6c64641726841748f903dcd9b8f4e1
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2025-05-20 19:03:42 -0700

    Make typescript happy

--- api/presign-upload.ts
+++ api/presign-upload.ts
@@ -1,8 +1,13 @@
 import { presignUpload } from 'fika-collect-lambda/dist/handler.js';
 import HttpError from 'fika-collect-lambda/dist/httpError.js';
 
+interface LambdaResponse {
+  statusCode: number;
+  body: string;
+}
+
 export async function POST(request: Request) {
-  const { statusCode, body } = await presignUpload({ body: await request.text() });
+  const { statusCode, body } = await presignUpload({ body: await request.text() }) as LambdaResponse;
 
   return new Response(body, {
     status: statusCode,
