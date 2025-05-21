f8d91a5ee71fc7c364da36845ef99f421d12034c
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2025-05-20 18:55:53 -0700

    Try cleaning up function error handling

--- api/presign-upload.ts
+++ api/presign-upload.ts
@@ -1,6 +1,14 @@
 import { presignUpload } from 'fika-collect-lambda/dist/handler.js';
+import HttpError from 'fika-collect-lambda/dist/httpError.js';
 
 export async function POST(request: Request) {
-  const body = await request.text();
-  return presignUpload({ body });
+  try {
+    const body = await request.text();
+    return presignUpload({ body });
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return new Response(error.message, { status: error.statusCode });
+    }
+    return new Response('Internal server error', { status: 500 });
+  }
 }
