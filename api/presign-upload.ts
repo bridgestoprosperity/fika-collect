9ec3284570b5ff1c0cc1b9f6e52b6490bb292b87
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2025-05-20 19:00:40 -0700

    Try improving api response

--- api/presign-upload.ts
+++ api/presign-upload.ts
@@ -2,13 +2,13 @@ import { presignUpload } from 'fika-collect-lambda/dist/handler.js';
 import HttpError from 'fika-collect-lambda/dist/httpError.js';
 
 export async function POST(request: Request) {
-  try {
-    const body = await request.text();
-    return presignUpload({ body });
-  } catch (error) {
-    if (error instanceof HttpError) {
-      return new Response(error.message, { status: error.statusCode });
+  const { statusCode, body } = await presignUpload({ body: await request.text() });
+
+  return new Response(body, {
+    status: statusCode,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
     }
-    return new Response('Internal server error', { status: 500 });
-  }
+  });
 }
