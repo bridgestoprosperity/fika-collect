7b474a8477d8f016cddea700d3ce05572f480cf9
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2025-05-20 18:47:37 -0700

    Change GET to POST

--- api/presign-upload.ts
+++ api/presign-upload.ts
@@ -1,6 +1,6 @@
 import { presignUpload } from 'fika-collect-lambda/dist/handler.js';
 
-export async function GET(request: Request) {
+export async function POST(request: Request) {
   const body = await request.text();
   return presignUpload({ body });
 }
