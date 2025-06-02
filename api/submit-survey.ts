76f1b9087ab3f6d8081f9ed7c36c365db44296f5
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2025-06-02 07:29:57 -0700

    Add submit survey endpoint

---
+++ api/submit-survey.ts
@@ -0,0 +1,19 @@
+import { submitSurvey } from 'fika-collect-lambda/dist/handler.js';
+import HttpError from 'fika-collect-lambda/dist/httpError.js';
+
+interface LambdaResponse {
+  statusCode: number;
+  body: string;
+}
+
+export async function POST(request: Request) {
+  const { statusCode, body } = await submitSurvey({ body: await request.text() }) as LambdaResponse;
+
+  return new Response(body, {
+    status: statusCode,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    }
+  });
+}
