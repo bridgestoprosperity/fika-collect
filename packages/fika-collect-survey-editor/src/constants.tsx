6dc16f22fc3472ddac05647c12809a989d955eb1
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2025-10-07 20:53:30 -0700

    Remove process from client-side app

--- packages/fika-collect-survey-editor/src/constants.tsx
+++ packages/fika-collect-survey-editor/src/constants.tsx
@@ -1,4 +1,3 @@
-import process from "process";
 export const S3_BASE_URL = "https://fika-collect.s3.us-west-1.amazonaws.com";
 export const MANIFEST_PATH = "surveys/manifest.json";
 
@@ -6,6 +5,4 @@ export const MANIFEST_PATH = "surveys/manifest.json";
 export const API_BASE_URL =
   typeof window !== "undefined" && window.location.hostname === "localhost"
     ? "http://localhost:3000/api/v1"
-    : ["development", "preview"].includes(process.env.VERCEL_ENV || "")
-    ? "https://staging.fikadigital.org/api/v1/"
-    : "https://app.fikadigital.org/api/v1";
+    : `${window.location.protocol}//${window.location.host}/api/v1`;
